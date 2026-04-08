a6b525c69cee09c2d29e4ae2267c507ca54c97ff
Adrian Solheim <adrsolheim@gmail.com>
2026-04-08 19:52:32 +0200

    Floating ball

--- window/src/main.zig
+++ window/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 //const window = @import("window");
 const rl = @import("raylib");
+const rg = @import("raygui");
 
 pub fn main() !void {
     const screenWidth = 800;
@@ -12,6 +13,7 @@ pub fn main() !void {
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+    var circle_x_pos: u16 = 0;
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
@@ -24,9 +26,12 @@ pub fn main() !void {
         //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
+        _ = rg.button(.init(24,24,120,30), "#191#Show message");
 
-        rl.clearBackground(.white);
+        rl.clearBackground(.pink);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+        rl.drawText("Heisann sveisan!", 190, 200, 20, .white);
+        rl.drawCircle(circle_x_pos, screenHeight/2, 20, .blue);
+        circle_x_pos = (circle_x_pos + 1) % screenWidth;
     }
 }
